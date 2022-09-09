921d22676ce285e6e4cc0a2a7ae1dcc0fb10ba2b
Betim Shala <betim_sh97@live.com>
2022-09-09 14:34:24 +0200

    Enable Cors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module mburoja-api
 go 1.18
 
 require (
+	github.com/gin-contrib/cors v1.4.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -130,6 +131,11 @@ func getSingleInvocation(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders: []string{"Content-Type,access-control-allow-origin, access-control-allow-headers"},
+	}))
 	router.Static("/audios", "./audios")
 
 	v1 := router.Group("/api/v1")
